f9b05e6d2c3a3fadc15a7fd4d0ab5ac9b9450c7a
Taki Academy <takiacademy@Nawfel-Sekrafi.local>
2022-12-05 13:30:53 +0100

    create a simple tour model

--- server.js
+++ server.js
@@ -16,6 +16,23 @@ mongoose
     console.log('DB connection successful!');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = 3000;
 
 console.log(process.env);
