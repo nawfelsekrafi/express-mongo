6f15a39e0a76f8efb44d574cac3219963e88f521
Taki Academy <takiacademy@Nawfel-Sekrafi.local>
2022-12-06 10:04:21 +0100

    added sorting, filtering and fields limiting to tous

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,36 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //1A) Filtering
+    const queryObj = { ...req.query };
+    const excludedFileds = ['page', 'sort', 'limit', 'fields'];
+    excludedFileds.forEach(el => delete queryObj[el]);
 
+    // 1B) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // EXECUTE QUERY
+
+    const tours = await query;
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestedTime,
